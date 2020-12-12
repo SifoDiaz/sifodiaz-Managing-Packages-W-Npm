fa6d46ae07f5ca36a3886b156217c0f5fad97b53
SifoDiaz <sifodiaz89@gmai.com>
2020-12-12 15:41:04 -0800

    More updates

--- myApp.js
+++ myApp.js
@@ -18,11 +18,11 @@ app.use("/public", express.static(__dirname + "/public"));
 //});
 
 app.get("/json", function(req, res) {
+  var jsonResponse = { message: "Hello json" };
   if (process.env.MESSAGE_STYLE === "uppercase") {
-    res.json({ message: "HELLO JSON" });
-  } else {
-    res.json({ message: "hello json" });
+    jsonResponse.message = jsonResponse.message.toUpperCase();
   }
+  res.json(jsonResponse);
 });
 
 module.exports = app;
