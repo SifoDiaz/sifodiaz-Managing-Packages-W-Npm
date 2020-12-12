70773b49fc7010465a18d870a791c63481c5310e
SifoDiaz <sifodiaz89@gmai.com>
2020-12-12 15:20:57 -0800

    Update to myapp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var bGround = require("fcc-express-bground");
+require("dotenv").config();
 
 bGround.log("Hello World");
 console.log("Hello World");
@@ -12,8 +13,16 @@ app.get("/", function(req, res) {
 app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
 
+//app.get("/json", function(req, res) {
+//  res.json({ message: "Hello json" });
+//});
+
 app.get("/json", function(req, res) {
-  res.json({ message: "Hello json" });
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "hello json" });
+  }
 });
 
 module.exports = app;
