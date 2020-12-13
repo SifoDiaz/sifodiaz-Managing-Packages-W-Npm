6c888083ce3a8e0719cdc95c4d8772b0e12c912b
SifoDiaz <sifodiaz89@gmai.com>
2020-12-12 20:09:47 -0800

    Data from post requests

--- myApp.js
+++ myApp.js
@@ -58,4 +58,8 @@ app.get("/name", function(req, res) {
   res.json({ name: req.query.first + " " + req.query.last });
 });
 
+app.post("/name", function(req, res) {
+  res.json({ name: req.body.first + " " + req.body.last });
+});
+
 module.exports = app;
